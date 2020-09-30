debbbf0dca6d7166b345d6500b2682b133835de6
Leo June Bedeo <jfljfb@gmail.com>
2020-09-30 14:57:14 +0800

    Initial commit. Created signup page.

---
+++ hotel_booking/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from hotel_booking.forms import SignUpForm
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'hotel_booking/index.html', {})
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'hotel_booking/signup.html', {'form': form})
